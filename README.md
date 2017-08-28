1dd1976c2fc828642e95f7cfc21326fd74f98a0d
kiq005 <kiq005@gmail.com>
2017-08-28 16:02:10 -0300

    New fixed AI, and game design

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # JSTicTacToe
 A Tic-Tac-Toe Game made in JavaScript, with some Artificial Intelligence Tests
 ## Current Game Modes
-The first player is are always human. The second player can be chosen as:
+The first player always is a human. The second player can be chosen as:
 - Human
-	- The oponent is a human player
+	- The opponent is a human player
 - Random Selection AI
 	- The Computer will just select a random move.
 - Try Win AI
 	- If a winning move is available, the computer do it, else, it use the 'Random Selection AI' strategy.
 - Minimax
-	- The computer will do a 3 level minimax search, if it doesn't suceed with a winning move, the computer will use the 'Try Win AI' strategy.
+	- The computer will do a 3 level minimax search, if it doesn't succeed with a winning move, the computer will use the 'Try Win AI' strategy.
+- Fixed
+	- The computer has a fixed plan, and use the 'Try Win AI" strategy.
--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
+/** 
+ * Global variables
+ **/
 var board = [0,0,0,
 			 0,0,0,
 			 0,0,0];
 var running = true;
 var xTurn = true;
+var folder = 'img';
 const FULL_RECURSIVE_MINIMAX = false;
 
+/*************
+ * Functions *
+ *************/
+
+/** 
+ * passTurn()
+ * Verify the board and check for a winner, if the game continue and is the computer
+ * turn, make a move. Then, pass the turn back to the player.
+ **/
 function passTurn(){
 	var win = verifyBoard();
 	if(win==-1){
@@ -30,6 +43,10 @@ function passTurn(){
 			document.getElementById('output').innerHTML = "Here goes Minimax!";
 			miniMax();
 		}
+		else if(opponent == 'fixed'){
+			document.getElementById('output').innerHTML = "U'll loooose!";
+			fixedAction();
+		}
 	}
 	else if(xTurn){
 		document.getElementById('output').innerHTML = "Do your move!";
@@ -39,6 +56,11 @@ function passTurn(){
 	}
 }
 
+/**
+ * rsaAction()
+ * Make a move based on the 'Random Selection AI' strategy, i.e., just select a 
+ * random square.
+ **/
 function rsaAction(){
 	var empty = [];
 	for(var i=0; i<9; ++i){
@@ -46,42 +68,89 @@ function rsaAction(){
 	}
 	clickSquare(empty[Math.floor(Math.random()*empty.length)]);
 }
-
+/**
+ * fixedAction()
+ * A fixed plan, based on game analysis.
+ **/
+function fixedAction(){
+	if(board[4]==0) clickSquare(4);// Priority to play in the middle
+	else if(board[0]==0 && board[2]==0 && board[6]==0 && board[8]==0) clickSquare(0);
+	else if(board[4]>0 && board[8]>0 && board[0]<0 && board[6]==0) clickSquare(6);
+	else if(board[4]<0 && board[0]>0 && board[8]>0 && board[1]==0) clickSquare(1);
+	else if(board[4]<0 && board[2]>0 && board[6]>0 && board[1]==0) clickSquare(1);
+	else twaAction();
+}
+/**
+ * twaAction()
+ * Make a move based on the 'Try to Win' strategy, i.e., just check if there is
+ * a winning move or a move that avoid loosing, if so, do it, else, just call rsaAction().
+ **/
 function twaAction(){
-	     if(Math.abs(board[0]+board[1])==2 && (board[2]==0)) clickSquare(2);
-	else if(Math.abs(board[1]+board[2])==2 && (board[0]==0)) clickSquare(0);
-	else if(Math.abs(board[0]+board[2])==2 && (board[1]==0)) clickSquare(1);
-	else if(Math.abs(board[3]+board[4])==2 && (board[5]==0)) clickSquare(5);
-	else if(Math.abs(board[5]+board[4])==2 && (board[3]==0)) clickSquare(3);
-	else if(Math.abs(board[3]+board[5])==2 && (board[4]==0)) clickSquare(4);
-	else if(Math.abs(board[6]+board[7])==2 && (board[8]==0)) clickSquare(8);
-	else if(Math.abs(board[6]+board[8])==2 && (board[7]==0)) clickSquare(7);
-	else if(Math.abs(board[7]+board[8])==2 && (board[6]==0)) clickSquare(6);
-	else if(Math.abs(board[0]+board[3])==2 && (board[6]==0)) clickSquare(6);
-	else if(Math.abs(board[0]+board[6])==2 && (board[3]==0)) clickSquare(3);
-	else if(Math.abs(board[3]+board[6])==2 && (board[0]==0)) clickSquare(0);
-	else if(Math.abs(board[1]+board[4])==2 && (board[7]==0)) clickSquare(7);
-	else if(Math.abs(board[1]+board[7])==2 && (board[4]==0)) clickSquare(4);
-	else if(Math.abs(board[4]+board[7])==2 && (board[1]==0)) clickSquare(1);
-	else if(Math.abs(board[2]+board[5])==2 && (board[8]==0)) clickSquare(8);
-	else if(Math.abs(board[2]+board[8])==2 && (board[5]==0)) clickSquare(5);
-	else if(Math.abs(board[5]+board[8])==2 && (board[2]==0)) clickSquare(2);
-	else if(Math.abs(board[0]+board[4])==2 && (board[8]==0)) clickSquare(8);
-	else if(Math.abs(board[0]+board[8])==2 && (board[4]==0)) clickSquare(4);
-	else if(Math.abs(board[4]+board[8])==2 && (board[0]==0)) clickSquare(0);
-	else if(Math.abs(board[2]+board[4])==2 && (board[6]==0)) clickSquare(6);
-	else if(Math.abs(board[6]+board[2])==2 && (board[4]==0)) clickSquare(4);
-	else if(Math.abs(board[4]+board[6])==2 && (board[2]==0)) clickSquare(2);
+	// Winning moves
+	if(board[0]+board[1]==-2 && (board[2]==0)) clickSquare(2);
+	else if(board[1]+board[2]==-2 && (board[0]==0)) clickSquare(0);
+	else if(board[0]+board[2]==-2 && (board[1]==0)) clickSquare(1);
+	else if(board[3]+board[4]==-2 && (board[5]==0)) clickSquare(5);
+	else if(board[5]+board[4]==-2 && (board[3]==0)) clickSquare(3);
+	else if(board[3]+board[5]==-2 && (board[4]==0)) clickSquare(4);
+	else if(board[6]+board[7]==-2 && (board[8]==0)) clickSquare(8);
+	else if(board[6]+board[8]==-2 && (board[7]==0)) clickSquare(7);
+	else if(board[7]+board[8]==-2 && (board[6]==0)) clickSquare(6);
+	else if(board[0]+board[3]==-2 && (board[6]==0)) clickSquare(6);
+	else if(board[0]+board[6]==-2 && (board[3]==0)) clickSquare(3);
+	else if(board[3]+board[6]==-2 && (board[0]==0)) clickSquare(0);
+	else if(board[1]+board[4]==-2 && (board[7]==0)) clickSquare(7);
+	else if(board[1]+board[7]==-2 && (board[4]==0)) clickSquare(4);
+	else if(board[4]+board[7]==-2 && (board[1]==0)) clickSquare(1);
+	else if(board[2]+board[5]==-2 && (board[8]==0)) clickSquare(8);
+	else if(board[2]+board[8]==-2 && (board[5]==0)) clickSquare(5);
+	else if(board[5]+board[8]==-2 && (board[2]==0)) clickSquare(2);
+	else if(board[0]+board[4]==-2 && (board[8]==0)) clickSquare(8);
+	else if(board[0]+board[8]==-2 && (board[4]==0)) clickSquare(4);
+	else if(board[4]+board[8]==-2 && (board[0]==0)) clickSquare(0);
+	else if(board[2]+board[4]==-2 && (board[6]==0)) clickSquare(6);
+	else if(board[6]+board[2]==-2 && (board[4]==0)) clickSquare(4);
+	else if(board[4]+board[6]==-2 && (board[2]==0)) clickSquare(2);
+	// Avoid loosing
+	else if(board[0]+board[1]==2 && (board[2]==0)) clickSquare(2);
+	else if(board[1]+board[2]==2 && (board[0]==0)) clickSquare(0);
+	else if(board[0]+board[2]==2 && (board[1]==0)) clickSquare(1);
+	else if(board[3]+board[4]==2 && (board[5]==0)) clickSquare(5);
+	else if(board[5]+board[4]==2 && (board[3]==0)) clickSquare(3);
+	else if(board[3]+board[5]==2 && (board[4]==0)) clickSquare(4);
+	else if(board[6]+board[7]==2 && (board[8]==0)) clickSquare(8);
+	else if(board[6]+board[8]==2 && (board[7]==0)) clickSquare(7);
+	else if(board[7]+board[8]==2 && (board[6]==0)) clickSquare(6);
+	else if(board[0]+board[3]==2 && (board[6]==0)) clickSquare(6);
+	else if(board[0]+board[6]==2 && (board[3]==0)) clickSquare(3);
+	else if(board[3]+board[6]==2 && (board[0]==0)) clickSquare(0);
+	else if(board[1]+board[4]==2 && (board[7]==0)) clickSquare(7);
+	else if(board[1]+board[7]==2 && (board[4]==0)) clickSquare(4);
+	else if(board[4]+board[7]==2 && (board[1]==0)) clickSquare(1);
+	else if(board[2]+board[5]==2 && (board[8]==0)) clickSquare(8);
+	else if(board[2]+board[8]==2 && (board[5]==0)) clickSquare(5);
+	else if(board[5]+board[8]==2 && (board[2]==0)) clickSquare(2);
+	else if(board[0]+board[4]==2 && (board[8]==0)) clickSquare(8);
+	else if(board[0]+board[8]==2 && (board[4]==0)) clickSquare(4);
+	else if(board[4]+board[8]==2 && (board[0]==0)) clickSquare(0);
+	else if(board[2]+board[4]==2 && (board[6]==0)) clickSquare(6);
+	else if(board[6]+board[2]==2 && (board[4]==0)) clickSquare(4);
+	else if(board[4]+board[6]==2 && (board[2]==0)) clickSquare(2);
 	else rsaAction();
 }
 
+/**
+ * shuffle(array)
+ * @return: array
+ * Take a array, shuffle the content, give it back.
+ */
 function shuffle(array){
 	var idx = array.length, temp, randIdx;
 	while(idx!=0){
-		// Pega um elemento aleatorio
+		// Get a random item from array
 		randIdx = Math.floor(Math.random()*idx);
 		idx-=1;
-		// Troca
+		// Switch
 		temp = array[idx];
 		array[idx]=array[randIdx];
 		array[randIdx]=temp;
@@ -89,6 +158,11 @@ function shuffle(array){
 	return array;
 }
 
+/**
+ * copy(array)
+ * @return: array
+ * Recive a array, and create a copy of it.
+ */
 function copy(array){
 	var newArray = [];
 	var i = array.length;
@@ -96,19 +170,26 @@ function copy(array){
 	return newArray;
 }
 
+/**
+ * miniMax()
+ * Make a move based on the 'Minimax' strategy, i.e., create a tree of possibilities
+ * and decide for the best move to get the best chance of winning.
+ */
 function miniMax(){
 	var possibleMoves = [];
 	var bestMove = null;
 	var bestPoints = 0;
 	var calls = 0;
+	// Get a list of empty squares
 	for(var i=0; i<9;++i){
 		if(board[i]==0){
 			possibleMoves.push(i);
 		}
 	}
-
+	// shuffle the possible Moves, to avoid getting the same 'best move' everytime.
 	possibleMoves = shuffle(possibleMoves);
 	for(var i=0; i< possibleMoves.length;++i){
+		// TODO: Avoid creating a copy of the board for every possible move.
 		var stepBoard = copy(board);
 		var move = possibleMoves[i];
 		stepBoard[move] = -1;
@@ -121,11 +202,11 @@ function miniMax(){
 	}
 
 	if(bestMove != null){
-		// Se encontrou um movimento bom, realiza
+		// If find a good move, do it
 		clickSquare(bestMove);
 	}
 	else{
-		// Se nao encontrou, realiza um movimento aleatorio (Isto nao deve acontecer)
+		// If doesn't find a good move, just call rsaAction(). (This shouldn't happen)
 		rsaAction();
 	}
 }
@@ -143,6 +224,7 @@ function miniMaxBoard(boardCheck, Oturn){
 			break;
 		case 0:
 			pts = 0;
+			// Goes one step further, with the second movement.
 			var possibleMoves = [];
 			for(var i=0; i<9;++i){
 				if(boardCheck[i]==0){
@@ -153,16 +235,20 @@ function miniMaxBoard(boardCheck, Oturn){
 				var move = possibleMoves[i];
 				var stepBoard = copy(board);
 				stepBoard[move] = (Oturn)?-1:1;
-				if(FULL_RECURSIVE_MINIMAX){//<-- Causa problemas de muita recursao
+				// TODO: Make the recursive method work!!!
+				if(FULL_RECURSIVE_MINIMAX){
+					// This is causing problems with a lot of recursion
 					pts += miniMax(stepBoard, !Oturn);
 				}
-				else{// Faz uma checagem de apenas 3 passos de profundidade
+				// Here we use a 'brute force' minimax, without recursion, but with only one more step
+				else{// Check the second movement.
 					var board2Res = countBoardPoints(stepBoard);
 					if(board2Res == -1)
 						pts+=1;
 					else if(board2Res == 1)
 						pts+=-1;
 					else{
+						// Goes one step further, with the third movement.
 						var possibleMoves3 = [];
 						for(var j=0; j<9;++j){
 							if(stepBoard[j]==0){
@@ -174,6 +260,7 @@ function miniMaxBoard(boardCheck, Oturn){
 							var stepBoard3 = copy(stepBoard);
 							stepBoard3[move] = (!Oturn)?-1:1;
 							var board3Res = countBoardPoints(stepBoard3);
+							// Check the third movement.
 							if(board3Res == -1){
 								pts+=1;
 							}
@@ -191,11 +278,12 @@ function miniMaxBoard(boardCheck, Oturn){
 	return pts;
 }
 /**
- * Retona:
- * 	-1 : O ganhou
- * 	 1 : X ganhou
- *	 3 : Empate
- *	 0 : Jogo nao terminou
+ * countBoardPoints(boardCheck)
+ * returns:
+ * 	-1 : O win
+ * 	 1 : X win
+ *	 3 : Draw
+ *	 0 : Game is running
  */
 function countBoardPoints(boardCheck){
 	if(Math.abs(boardCheck[0]+boardCheck[1]+boardCheck[2])==3)	return boardCheck[0];
@@ -286,12 +374,12 @@ function verifyBoard(){
 
 function getXImg(){
 	var files = ["X1", "X2", "X3", "X4"];
-	return "img/"+files[Math.floor(Math.random()*files.length)]+".png";
+	return folder+"/"+files[Math.floor(Math.random()*files.length)]+".png";
 }
 
 function getOImg(){
 	var files = ["O1", "O2", "O3", "O4"];
-	return "img/"+files[Math.floor(Math.random()*files.length)]+".png";
+	return folder+"/"+files[Math.floor(Math.random()*files.length)]+".png";
 }
 
 function selectRandomBoard(){
@@ -319,19 +407,24 @@ function clickSquare(pos){
 }
 
 function resetBoard(){
+	var design = document.gameDesign.list2.value;
 	document.getElementById('output').innerHTML = "X Starts!";
 	board = [0,0,0,
 			 0,0,0,
 			 0,0,0];
 	running = true;
 	xTurn = true;
+	folder = ["img", "img_obj", "img_sing"][design];
 	document.getElementById('board').className = selectRandomBoard();
 	for(var i=0;i<9;++i){
-		document.getElementById('p'+i).src = "img/Space.png";
+		document.getElementById('p'+i).src = folder+"/Space.png";
 		document.getElementById('p'+i).className = "square";
 	}
 }
 
+/** 
+ * Define buttons actions
+ **/
 document.getElementById('board').className = selectRandomBoard();
 document.getElementById('p0').onclick = function(event){clickSquare(0)}
 document.getElementById('p1').onclick = function(event){clickSquare(1)}
